239ebb384ab1d43f5bbeee7a47d450ccf2c6c56b
tvkanters <tvkanters@gmail.com>
2013-12-11 18:08:03 +0100

    Fixed typos in evaluator.

--- InformationRetrieval/src/com/uva/ir/Evaluator.java
+++ InformationRetrieval/src/com/uva/ir/Evaluator.java
@@ -23,133 +23,119 @@ import com.uva.ir.retrieval.models.BM25RetrievalModel;
 import com.uva.ir.retrieval.models.IntersectionRetrievalModel;
 import com.uva.ir.retrieval.models.RetrievalModel;
 import com.uva.ir.retrieval.models.TfIdfRetrievalModel;
-import com.uva.ir.retrieval.models.VectorSpaceRetrievalModel;
 import com.uva.ir.utils.FileManager;
 
+/**
+ * A class used for evaluating the results of retrieval models through our own test and TREC's tests.
+ */
 public class Evaluator {
 
-    
-    
     public static void main(final String[] args) {
         RetrievalModel rtModel = null;
         rtModel = new IntersectionRetrievalModel();
         Retriever retriever = generateRetriever(new StemmingPreprocessor(), rtModel);
-        
+
         createTrecFile(retriever, rtModel);
         System.out.println(evaluateWithOfflineTrec());
-//        evaluateTRAC();
+        // evaluateTrec();
     }
-    
-//    BM25 with B:0.4 K: 4.0
-//    0.7697
-    
-//    BEST
-//    BM25 with B:0.6 K: 10.0
-//    0.7871
-    
-//    BEST
-//    BM25 with B:0.45 K: 11.0
-//    0.7871
-    private static void evaluateTRAC() {
-        
+
+    private static void evaluateTrec() {
+
         double bestResult = 0;
         RetrievalModel best = null;
         double worstResult = Double.MAX_VALUE;
         RetrievalModel worst = null;
         RetrievalModel rtModel = null;
-        
+
         double stepSizeB = 0.05;
         int stepSizeK = 1;
-        for(double b = stepSizeB; b <= 1; b+=stepSizeB) {
-            System.out.println("----- " +b + " ------");
-            for(int k = 1; k <= 20; k+=stepSizeK) {
+        for (double b = stepSizeB; b <= 1; b += stepSizeB) {
+            System.out.println("----- " + b + " ------");
+            for (int k = 1; k <= 20; k += stepSizeK) {
 
                 rtModel = new BM25RetrievalModel(b, k);
                 Retriever retriever = generateRetriever(new StemmingPreprocessor(), rtModel);
-                
+
                 createTrecFile(retriever, rtModel);
                 double result = evaluateWithOfflineTrec();
-                
-                System.out.println("& B: " +b + " K: " +k +" & " +result + " \\\\");
-                
-                if(result > bestResult) {
+
+                System.out.println("& B: " + b + " K: " + k + " & " + result + " \\\\");
+
+                if (result > bestResult) {
                     bestResult = result;
                     best = rtModel;
                 }
-                
-                if(result < worstResult) {
+
+                if (result < worstResult) {
                     worstResult = result;
                     worst = rtModel;
                 }
             }
         }
-        
-       System.out.println("BEST");
-       System.out.println(best.getModelName());
-       System.out.println(bestResult);
-       
-       System.out.println();
-       System.out.println("WORST");
-       System.out.println(worst.getModelName());
-       System.out.println(worstResult);
+
+        System.out.println("BEST");
+        System.out.println(best.getModelName());
+        System.out.println(bestResult);
+
+        System.out.println();
+        System.out.println("WORST");
+        System.out.println(worst.getModelName());
+        System.out.println(worstResult);
     }
-    
+
     private static void createTrecFile(Retriever retriever, RetrievalModel rtModel) {
         PrintWriter writer;
         try {
             writer = new PrintWriter("trec_eval.9.0/result.txt", "UTF-8");
 
-            List<QueryResultEntry> resultList = retriever.executeQuery(Query.getQueryForID(QueryID.QUERY_6));
+            List<QueryResultEntry> resultList = retriever.executeQuery(Query
+                    .getQueryForID(QueryID.QUERY_6));
             List<String> result = generateTracData(resultList, QueryID.QUERY_6);
-            for(String s : result) {
+            for (String s : result) {
                 writer.println(s);
             }
-            
+
             resultList = retriever.executeQuery(Query.getQueryForID(QueryID.QUERY_7));
             result = generateTracData(resultList, QueryID.QUERY_7);
-            for(String s : result) {
+            for (String s : result) {
                 writer.println(s);
             }
             writer.close();
-        
+
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-    
+
     private static double evaluateWithOfflineTrec() {
         double result = 0;
-        try
-        {
-           Runtime rt = Runtime.getRuntime();
-           String cmdString = "trec_eval.9.0/trec_eval -q -c -M1000 trec_eval.9.0/qrels.txt trec_eval.9.0/result.txt";
-           Process pr = rt.exec(cmdString);
+        try {
+            Runtime rt = Runtime.getRuntime();
+            String cmdString = "trec_eval.9.0/trec_eval -q -c -M1000 trec_eval.9.0/qrels.txt trec_eval.9.0/result.txt";
+            Process pr = rt.exec(cmdString);
 
-           BufferedReader input = new BufferedReader(new InputStreamReader(
-                 pr.getInputStream()));
+            BufferedReader input = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 
-           String line = null;
+            String line = null;
 
-           while ((line = input.readLine()) != null)
-           {
-              if(line.startsWith("map")) {
-                  String[] r = line.replaceAll("\\s+", " ").split(" ");
-                  result = Double.parseDouble(r[2]);
-              }
-           }
+            while ((line = input.readLine()) != null) {
+                if (line.startsWith("map")) {
+                    String[] r = line.replaceAll("\\s+", " ").split(" ");
+                    result = Double.parseDouble(r[2]);
+                }
+            }
 
-           int exitVal = pr.waitFor();
+            int exitVal = pr.waitFor();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
         }
-        catch (Exception e)
-        {
-           System.out.println(e.toString());
-           e.printStackTrace();
-        }
-        
+
         return result;
     }
-    
+
     private static void evaluateLupti() {
         RetrievalModel rtModel = new BM25RetrievalModel();
         Retriever retriever = generateRetriever(new SimplePreprocessor(), rtModel);
@@ -197,16 +183,18 @@ public class Evaluator {
         final InvertedIndex invertedIndex = new InvertedIndex(preprocessor, files);
         return new Retriever(invertedIndex, retrievalModel);
     }
-    
-    //<queryID> Q0 <documentID> <rank> <score> <runID>
-    private static List<String> generateTracData(List<QueryResultEntry> queryResults, QueryID queryID) {
+
+    // <queryID> Q0 <documentID> <rank> <score> <runID>
+    private static List<String> generateTracData(List<QueryResultEntry> queryResults,
+            QueryID queryID) {
         List<String> result = new ArrayList<String>();
-        
+
         int rank = 0;
-        for(QueryResultEntry q : queryResults) {
-            result.add(queryID.getValue() +" Q0 " +q.getDocument().getBaseName() +" " +rank++ +" " +q.getScore() +" 1");
+        for (QueryResultEntry q : queryResults) {
+            result.add(queryID.getValue() + " Q0 " + q.getDocument().getBaseName() + " " + rank++
+                    + " " + q.getScore() + " 1");
         }
-        
+
         return result;
     }
 }
